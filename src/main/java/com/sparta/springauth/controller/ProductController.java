eb56f6e78cd548a8317c4f073e80750f2631c77d
박수민 <allan1593@naver.com>
2024-02-06 16:07:58 +0900

    Spring Security 로그인

--- src/main/java/com/sparta/springauth/controller/ProductController.java
+++ src/main/java/com/sparta/springauth/controller/ProductController.java
@@ -1,7 +1,7 @@
 package com.sparta.springauth.controller;
 
-import com.sparta.springauth.entity.User;
-import jakarta.servlet.http.HttpServletRequest;
+import com.sparta.springauth.security.UserDetailsImpl;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProductController {
 
     @GetMapping("/products")
-    public String getProducts(HttpServletRequest req) {
-        System.out.println("ProductController.getProducts : 인증 완료");
-        User user = (User) req.getAttribute("user");
-        System.out.println("user.getUsername() = " + user.getUsername());
+    public String getProducts(@AuthenticationPrincipal UserDetailsImpl userDetails) {
 
         return "redirect:/";
     }
