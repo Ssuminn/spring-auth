26852e8bd5339f69797cfdd43640329f29e84a33
박수민 <allan1593@naver.com>
2024-02-06 18:18:40 +0900

    접근 불가 페이지

--- src/main/java/com/sparta/springauth/controller/ProductController.java
+++ src/main/java/com/sparta/springauth/controller/ProductController.java
@@ -1,6 +1,9 @@
 package com.sparta.springauth.controller;
 
+import com.sparta.springauth.entity.UserRoleEnum;
 import com.sparta.springauth.security.UserDetailsImpl;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +15,17 @@ public class ProductController {
 
     @GetMapping("/products")
     public String getProducts(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        return "redirect:/";
+    }
+
+    @Secured(UserRoleEnum.Authority.ADMIN) // 관리자용
+    @GetMapping("/products/secured")
+    public String getProductsByAdmin(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        System.out.println("userDetails.getUsername() = " + userDetails.getUsername());
+        for (GrantedAuthority authority : userDetails.getAuthorities()) {
+            System.out.println("authority.getAuthority() = " + authority.getAuthority());
+        }
 
         return "redirect:/";
     }
